68c61da1065693d84a8b569e7ce888ae2d9a8026
Alexander Forselius <drsounds@gmail.com>
2021-06-26 15:32:59 +0200

    Changed port to 8080

--- index.js
+++ index.js
@@ -101,4 +101,4 @@ app.get('/', function (req, res) {
 });
 module.exports = app;
 console.log("RUNNING")
- let server = http.createServer(app).listen(2858);
+let server = http.createServer(app).listen(8080);
